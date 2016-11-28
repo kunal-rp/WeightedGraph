77d3ca26d0c7f2ef58041c99bf43729dc9133199
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-11-28 13:50:35 -0800

    Created basic Vertex and Edge Skeleton

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 /**
  * Created by KP Admin on 11/28/2016.
  */
@@ -5,6 +7,24 @@ public class Main {
 
     public static void main(String args[]){
 
-        System.out.print("Hello");
+
+    }
+
+}
+class Vertex{
+
+    private char letter;
+    private Iterator<Edge> edges;
+
+}
+
+class Edge{
+
+    private Vertex vertex;
+    private int weight;
+
+    public Edge(Vertex vertex, int weight){
+        this.vertex = vertex;
+        this.weight = weight;
     }
 }
