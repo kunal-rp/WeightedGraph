833fe4cdc8360af75d950736fa81b2afd580b8fa
kunal-rp <kunal.r.rajpurohit@gmail.com>
2016-11-28 16:38:12 -0800

    Finished basic Classes - Graphs, Ver., and Edge also added and tested printing functionality also added adding and removing functionality

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,77 @@ public class Main {
 
     public static void main(String args[]){
 
+        Graph graph = new Graph();
+        graph.add('A');
+        graph.add('B');
+        graph.add('C');
+        graph.addEdge('A','B',3);
+        graph.addEdge('B','C',4);
+        graph.addEdge('B','A',4);
+        graph.print();
+        graph.remove('C');
+        graph.print();
+    }
+
+}
+
+class Graph{
+
+    private ArrayList<Vertex> vertexes = new ArrayList<Vertex>();
+
+    public void add(char letter){
+        Vertex vertex = new Vertex(letter);
+        vertexes.add(vertex);
+    }
+
+    public void addEdge(char startChar, char endChar, int weight){
+
+        Vertex start = new Vertex('/');
+        Vertex end = new Vertex('/');
+        Iterator<Vertex> vertexList = vertexes.iterator();
+        while(vertexList.hasNext()){
+            Vertex temp = vertexList.next();
+            if(temp.getLetter() == startChar){
+                start = temp;
+            }
+            if(temp.getLetter() == endChar){
+                end = temp;
+            }
+        }
+        start.addEdge(end, weight);
+    }
+
+    public void remove(char letter){
+        Vertex vertex = new Vertex(letter);
+        Iterator<Vertex> vertexList = vertexes.iterator();
+        while(vertexList.hasNext()){
+            Vertex temp = vertexList.next();
+            if(temp.getLetter() == vertex.getLetter()){
+                vertexList.remove();
+            }
+            else{
+                temp.removeEdge(vertex);
+            }
+        }
 
     }
+
+    public void print(){
+        Iterator<Vertex> vertexList = vertexes.iterator();
+        while(vertexList.hasNext()){
+            Vertex temp = vertexList.next();
+            temp.printEdges();
+            System.out.println();
+        }
+
+    }
+
 
 }
 class Vertex{
 
     private char letter;
-    private ArrayList<Edge> edges ;
+    private ArrayList<Edge> edges = new ArrayList<Edge>() ;
     private boolean visited;
 
     public Vertex(char letter){
@@ -35,7 +98,19 @@ class Vertex{
         visited = true;
     }
 
+    public void removeEdge(Vertex vertex){
+        Iterator<Edge> edgelist = edges.iterator();
+        while(edgelist.hasNext()){
+            Edge temp = edgelist.next();
+            if(temp.getVertex().getLetter() == vertex.getLetter()){
+                edgelist.remove();
+                break;
+            }
+        }
+    }
+
     public void printEdges(){
+        System.out.print(letter + " :");
         Iterator<Edge> edgelist = edges.iterator();
         while(edgelist.hasNext()){
             Edge temp = edgelist.next();
@@ -73,6 +148,19 @@ class Vertex{
             Edge temp = edgelist.next();
             if(temp.getVertex().isVisited() == false){
                 result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public Vertex getUnvisitedVertex(){
+        Vertex result = new Vertex('/');
+        Iterator<Edge> edgelist = edges.iterator();
+        while(edgelist.hasNext() ) {
+            Edge temp = edgelist.next();
+            if(temp.getVertex().isVisited() == false){
+                result = temp.getVertex();
                 break;
             }
         }
